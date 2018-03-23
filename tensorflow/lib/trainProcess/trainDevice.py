657e2daa60437ad9a163eea44ea4a627a1584953
SlipknotTN <micheletoni87@tin.it>
2018-03-23 18:51:47 +0100

    Fixed train device selection with multi gpu

--- tensorflow/lib/trainProcess/trainDevice.py
+++ tensorflow/lib/trainProcess/trainDevice.py
@@ -3,12 +3,14 @@ import os
 
 def selectTrainDevice(useGpu):
     """
-    Select cpu/gpu as train device
+    Select cpu/gpu as train device (single GPU support)
     :param useGpu:
     :return:
     """
+    if len(useGpu.split(",")) > 1:
+        raise Exception("We support only 1 GPU")
     if useGpu is not None:
-        trainDevice = '/gpu:' + useGpu
+        trainDevice = '/gpu:0'
         print('Train device: ' + useGpu)
         os.environ['CUDA_VISIBLE_DEVICES'] = useGpu
     else:
